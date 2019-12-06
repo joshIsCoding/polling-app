deb194913540b57ec709534678de00ae17ff0364
joshIsCoding <yes_you_do@hotmail.com>
2019-12-06 16:40:30 +0000

    Write and run CreateResponses migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_163537) do
+ActiveRecord::Schema.define(version: 2019_12_06_163836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2019_12_06_163537) do
     t.text "text", null: false
     t.integer "poll_id"
     t.index ["poll_id"], name: "index_questions_on_poll_id"
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "answer_choice_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["answer_choice_id"], name: "index_responses_on_answer_choice_id"
+    t.index ["user_id"], name: "index_responses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
