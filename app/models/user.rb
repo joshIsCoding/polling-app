2764d7adebb80f7524e47a4d9f67d1753244c632
joshIsCoding <yes_you_do@hotmail.com>
2019-12-06 19:20:03 +0000

    Write User model and basic associations

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :username, uniqueness: true, presence: true
+
+  has_many(
+    :authored_polls,
+    class_name: 'Poll',
+    foreign_key: :author_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :responses,
+    class_name: 'Response',
+    foreign_key: :user_id,
+    primary_key: :id
+  )
+end
