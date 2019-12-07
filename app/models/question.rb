9645f6a13826be2cd730636fe031fcab56ba7cec
joshIsCoding <yes_you_do@hotmail.com>
2019-12-07 22:54:43 +0000

    Add Question#results to generate hashmap of the answer choices and corresponding response counts

--- app/models/question.rb
+++ app/models/question.rb
@@ -16,4 +16,15 @@ class Question < ApplicationRecord
   )
 
   has_many :responses, through: :answer_choices, source: :responses
+
+  def results
+    results_ar = self.answer_choices.select("answer_text, COUNT(responses.id) AS response_count")
+    .left_outer_joins(:responses)
+    .group("answer_choices.id")
+
+    results_ar.inject({}) do |results, record|
+      results[record.answer_text] = record.response_count
+      results
+    end
+  end
 end
